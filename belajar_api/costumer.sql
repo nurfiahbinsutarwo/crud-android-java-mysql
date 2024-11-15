f9d6989993845972eaedb18a0f774b2cefcca321
nurfiahbinsutarwo <nurfiahsutarwo@gmail.com>
2024-11-15 21:29:47 +0700

    costumer.sql

---
+++ belajar_api/costumer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `costumer` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `alamat` text NOT NULL,
+  `hp` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
